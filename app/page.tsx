657a0b349a52da23c71eed197dd42bb474329c7f
OnlineNets <nets01929@gmail.com>
2023-08-21 15:13:56 +0300

    (feat) Setup and configure authentication using the Clerk signin and signup components

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
-        <h1 class="text-6xl mb-4">The best Journal app, period.</h1>
-        <p class="text-2xl text-white/60 mb-4">
+        <h1 className="text-6xl mb-4">The best Journal app, period.</h1>
+        <p className="text-2xl text-white/60 mb-4">
           This is the best app for tracking your mood throughout your life, all
           you have to do is be honest
         </p>
