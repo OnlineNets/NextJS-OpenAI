bee6152dcf5a70111988eaa0fe8c43a6d23ffd16
OnlineNets <nets01929@gmail.com>
2023-09-07 15:28:15 +0300

    (feat) Add to the ui the ability to auto refresh the analysis after auto save, but the results from the analysis seem to be broken sometimes, shows uknown of the type string

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,6 +1,8 @@
+import EntryCard from "@/components/EntryCard";
 import { getUserByClerkId } from "@/utils/auth"
 import { prisma } from "@/utils/db";
 import { NextResponse } from "next/server";
+import { analyse } from "@/utils/ai"
 
 export const PATCH = async (request, { params }) => {
   const { content } = await request.json()
@@ -17,5 +19,20 @@ export const PATCH = async (request, { params }) => {
     }
   })
 
-  return NextResponse.json({ data: updatedEntry })
+  const analysis = await analyse(updatedEntry.content)
+
+  const updated = await prisma.analysis.upsert({
+    where: {
+      entryId: updatedEntry.id
+    },
+    create: {
+      entryId: updatedEntry.id,
+      ...analysis
+    },
+    update: analysis
+  })
+
+  console.log('>>> updated: ', updated)
+
+  return NextResponse.json({ data: { ...updatedEntry, analysis: updated } })
 }
