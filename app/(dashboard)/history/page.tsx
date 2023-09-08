061890fc9d4e8b6dd0c818eed86f7a0da3542d0d
OnlineNets <nets01929@gmail.com>
2023-09-08 17:51:56 +0300

    (feat) Add a relation to user on the analysis to get all the analysis of a user

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,3 +1,12 @@
+import { prisma } from '@/utils/db'
+
+const getData = async () => {
+  const user = await getUserByClerkId()
+  const analyses = await prisma.analysis.findMany({
+    where: {},
+  })
+}
+
 const History = () => {
   return <div>history</div>
 }
