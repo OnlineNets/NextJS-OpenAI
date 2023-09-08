f7d3316beef803250705a56e2b4c87f4a12c5937
OnlineNets <nets01929@gmail.com>
2023-09-08 17:00:23 +0300

    (feat) Use open ai embedings stored in memory to provide answer to any questions the user might have about his journal entries

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,22 +1,32 @@
 'use client'
 
+import { askQuestion } from '@/utils/api'
 import { useState } from 'react'
 
 const Question = () => {
-  const [value, setValue] = useState()
+  const [value, setValue] = useState('')
+  const [loading, setLoading] = useState('')
+  const [response, setResponse] = useState('')
 
   const onChangeHandler = (event) => {
     setValue(event.target.value)
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
+
+    setLoading(true)
+    const answer = await askQuestion(value)
+    setResponse(answer)
+    setValue('')
+    setLoading(false)
   }
 
   return (
     <div>
       <form action="" onSubmit={handleSubmit}>
         <input
+          disabled={loading}
           className="border border-black/20 px-4 py-2 text-lg rounded-lg"
           type="text"
           placeholder="Ask a question"
@@ -30,6 +40,8 @@ const Question = () => {
           Ask
         </button>
       </form>
+      {loading && <div>...loading</div>}
+      {response && <div>{response}</div>}
     </div>
   )
 }
