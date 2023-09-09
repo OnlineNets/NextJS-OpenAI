d5f0531c8213686adb1fd84a13e95681293fb826
OnlineNets <nets01929@gmail.com>
2023-09-09 12:54:18 +0300

    (feat) Add the ability to access from the history page, the sentiment score of each journal entry as well as a computed average that will be used to render a chart

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,13 +1,26 @@
 import { prisma } from '@/utils/db'
+import { getUserByClerkId } from '@/utils/auth'
 
 const getData = async () => {
   const user = await getUserByClerkId()
   const analyses = await prisma.analysis.findMany({
-    where: {},
+    where: {
+      userId: user.id,
+    },
+    select: {
+      sentimentScore: true,
+    },
   })
+
+  const sum = analyses.reduce((all, current) => all + current.sentimentScore)
+  const average = Math.round(sum / analyses.length)
+
+  return { analyses, average }
 }
 
-const History = () => {
+const History = async () => {
+  const { avg, analyses } = await getData()
+  console.log('>>> analyses', analyses)
   return <div>history</div>
 }
 
